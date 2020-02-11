c040b3064629e5e585c99987f3e8521040a83daf
Ali Jahan <ajaha004@ucr.edu>
2020-02-11 12:28:14 -0800

    Generates compute benchmarks that include all Integer and Floating point instructions. PTX 6.3 ISA is used in ISA.py

--- nanobenchmarks/compute/generate_int_arith_benchmarks.py
+++ nanobenchmarks/compute/generate_int_arith_benchmarks.py
@@ -1,26 +1,31 @@
 from ISA import *
 import sys
 
+div_by_zero = ["div", "rem"]
 
-def generate_code(nb_instr, instr, template_code):
-	instr_type = TYPES[ISA_table[instr][1]][0]
+def generate_code(nb_instr, instr, template_code, instr_type):
 	source_width = int(instr_type[1:])
 	source_s_u = instr_type[:1]
 	dest_width = source_width*ISA_table[instr][2]
 	assert dest_width <= 64
 	instr_nb_source_operands = ISA_table[instr][0]-1
-	generated_instr_code = "\tasm volatile(\".reg ."+source_s_u+str(dest_width)+" %r12;\\n\"\n"
+	generated_instr_code = "\tasm volatile(\".reg ."+source_s_u+str(dest_width)+" %r0;\\n\"\n"
 
 	for i in range(instr_nb_source_operands):
-		 generated_instr_code += "\t\t\t\".reg ."+source_s_u+str(source_width)+" %r"+str(13+i)+";\\n\"\n"
+		 generated_instr_code += "\t\t\t\".reg ."+source_s_u+str(source_width)+" %r"+str(1+i)+";\\n\"\n"
+
+	if instr in div_by_zero:
+		instr_nb_source_operands -= 1
 
 	for i in range(nb_instr):
-		
-		generated_instr_code += "\t\t\t\""+instr+"."+source_s_u+str(source_width)+" %r12, "
+		generated_instr_code += "\t\t\t\""+instr+"."+source_s_u+str(source_width)+" %r0, "
 		instr_operands = ""
 		for j in range(instr_nb_source_operands):
-			instr_operands += "%r"+str(13+j)+", "
-		instr_operands = instr_operands[:-2]+";\\n\"\n"
+			instr_operands += "%r"+str(1+j)+", "
+		instr_operands = instr_operands[:-2]
+		if instr in div_by_zero:
+			instr_operands += ", 10"
+		instr_operands += ";\\n\"\n"
 		generated_instr_code += instr_operands
 	generated_instr_code += ");\n"
 	template_code = template_code.replace("INSERT_CODE_HERE", generated_instr_code)
@@ -33,10 +38,15 @@ def main():
 	template_code  = f.read()
 	f.close()
 	for i in ISA_table:
-		code = generate_code(nb_instr, i, template_code)
-		f = open(i+".cu",'w')
-		f.write(code)
-		f.close()
+		i_types = TYPES[ISA_table[i][1]]
+		source_width = ISA_table[i][2]	
+		for i_type in i_types:
+			if source_width==1:
+				#print("instr:"+str(i)+" type:"+str(i_type))
+				code = generate_code(nb_instr, i, template_code, i_type)
+				f = open(i+"."+str(i_type)+".cu",'w')
+				f.write(code)
+				f.close()
 
 if __name__ == "__main__":
 	main()			
--- nanobenchmarks/compute/run.sh
+++ nanobenchmarks/compute/run.sh
@@ -4,7 +4,7 @@ NC='\033[0m'
 
 
 GPU_ARCH=SM2_GTX480
-NB_INSTR=4096
+NB_INSTR=1
 
 if [ -z ${GPGPUSIM_ROOT} ]; then 
 	echo "Please set GPGPU-SIM environment variables"; 
@@ -12,7 +12,7 @@ if [ -z ${GPGPUSIM_ROOT} ]; then
 fi
 
 make clean
-rm -rf all_stats _cuobjdump* *.log _ptx* *.pyc gpgpu* config* *.xml _app_cuda_version* *.ptx* checkpoint*
+rm -rf all_stats _cuobjdump* *.log _ptx* *.pyc gpgpu* config* *.xml _app_cuda_version* *.ptx* checkpoint* __pycache__
 
 echo -e "${RED}Generating benchmarks with $NB_INSTR instructions${NC}"
 python generate_int_arith_benchmarks.py $NB_INSTR
@@ -22,7 +22,7 @@ make -j$(nproc) > /dev/null
 
 cp $GPGPUSIM_ROOT/configs/tested-cfgs/$GPU_ARCH/* ./ && echo -e "${RED}$GPU_ARCH is copied as gpgpusim arch confing file${NC}"
 
-printf "INSTR,IC_H,IC_M,DC_RH,DC_RM,DC_WH,DC_WM,TC_H,TC_M,CC_H,CC_M,SHRD_ACC,REG_R,REG_W,NON_REG_OPs,SFU_ACC,SP_ACC,FPU_ACC,TOT_INST,FP_INT,DRAM_RD,DRAM_WR,DRAM_PRE,L2_RH,L2_RM,L2_WH,L2_WM,PIPE,NOC_A,IDLE_CORE_N,\n" >> all_stats;
+printf "INSTR_NAME, LINE#, INSTR_CNT, T_DECODE, T_ALU, T_FP, T_INT_MUL, T_TEX, T_FP_MUL, T_TRANS, T_INT_DIV, T_FP_DIV, T_INT, T_SP, T_SFU, T_DP, T_FP_SQRT, T_FP_LG, T_FP_EXP, T_FP_SIN, T_COLL_UNT, T_TENSOR, NB_MEM_ACC, NB_RF_RD, NB_RF_WR, NB_NON_RF_OPRND, T_INT_MUL24, T_INT_MUL32, DIVERGENCE, NB_LD, NB_ST, IS_SHMEM_INST, IS_SSTAR_INST, IS_TEX_INST, IS_CONST_INST, IS_PARAM_INST, NB_LOCAL_MEM_RD, NB_LOCAL_MEM_WR, NB_TEX_MEM, NB_CONST_MEM, NB_GLOB_MEM_RD, NB_GLOB_MEM_WR, IC_HIT, IC_MISS, DC_L1_LD, DC_L1_ST,\n" >> all_stats;
 
 echo -e "${RED}Running benchmarks...${NC}"
 for i in *.cu; 
@@ -30,9 +30,8 @@ do
 	echo $i; 
 	./${i::-3} > /dev/null && \
 	printf "%s," ${i::-3} >> all_stats && \
-	sed -n 2p gpgpusim_power_report__*.log >> all_stats;
-	rm *.log; 
+	sed -n 2p gpgpu_inst_stats.txt >> all_stats;
 done
 
 
-
+#tail -n +2 gpgpu_inst_stats.txt | awk '{print $2}' | xargs -I% sed -n %p vadd.1.sm_30.ptx | cut -d '.' -f1

415d8ea7d0defc1ca3c828912111114203a1f9fd
Ali Jahan <ajaha004@ucr.edu>
2020-02-27 16:44:03 -0800

    logic_shift benchmarks added, compiled but not tested on hardware yet

--- nanobenchmarks/logic_shift/generate_benchmarks.py
+++ nanobenchmarks/logic_shift/generate_benchmarks.py
@@ -1,37 +1,54 @@
 from ISA import *
 import sys
+from random import random
+import argparse
 
+BASE_REG_NUM = 21
 # Main function for code generation, given instr name and type
 def generate_code(nb_instr, instr, instr_type, nb_sources, last_op_type, template_code):
 
 	# Register declaration
 	nb_op = 0
-	generated_instr_code = "\tasm volatile(\".reg "+instr_type+" %r"+str(nb_op)+";\\n\"\n"
+	register_code = "\tasm volatile(\".reg "+instr_type+" %r"+str(BASE_REG_NUM)+";\\n\"\n"
 	nb_op += 1
+	not_pred = False
+	if not instr_type == ".pred":
+		not_pred = True
+		instr_width = int(instr_type[2:])
 	for i in range(nb_sources-1):
-		generated_instr_code += "\t\t\t\".reg "+instr_type+" %r"+str(nb_op)+";\\n\"\n"
+		register_code += "\t\t\t\".reg "+instr_type+" %r"+str(BASE_REG_NUM+i+1)+";\\n\"\n"
+		if not_pred:
+			register_code += "\"mov"+instr_type+" %r"+str(BASE_REG_NUM+i+1)+", "+str(int(1000000*random()) % 2**min(32, instr_width))+";\\n\"\n"
 		nb_op += 1
 	if last_op_type == "same":
-		generated_instr_code += "\t\t\t\".reg "+instr_type+" %r"+str(nb_op)+";\\n\"\n"
+		register_code += "\t\t\t\".reg "+instr_type+" %r"+str(BASE_REG_NUM+nb_sources)+";\\n\"\n "
+		if not_pred:
+			register_code += "\"mov"+instr_type+" %r"+str(BASE_REG_NUM+nb_sources)+", "+str(int(1000000*random()) % 2**min(32, instr_width))+";\\n\"\n" 
 	else:
-		generated_instr_code += "\t\t\t\".reg "+last_op_type+" %r"+str(nb_op)+";\\n\"\n"
+		lo_width = int(last_op_type[2:])
+		register_code += "\t\t\t\".reg "+last_op_type+" %r"+str(BASE_REG_NUM+nb_sources)+";\\n\"\n"
+		if not_pred:
+			register_code += "\"mov.u32 %r"+str(BASE_REG_NUM+nb_sources)+", "+str(int(1000000*random()) % 2**min(32, lo_width))+";\\n\"\n" 
 
+	register_code += ");\n"
+	template_code = template_code.replace("REGISTER_CODE", register_code)
+	compute_code = "\tasm volatile(\n"; 
 	# Generate code to instruction	
 	for i in range(nb_instr):
 		# Add instruction and the dest register which always is r0
-		generated_instr_code += "\t\t\t\""+instr+instr_type+" %r0, "
+		compute_code += "\t\t\t\""+instr+instr_type+" %r"+str(BASE_REG_NUM)+", "
 		
 		for j in  range(1,nb_sources+1):	
-			generated_instr_code +=	"%r"+str(j)+", "
+			compute_code +=	"%r"+str(BASE_REG_NUM+j)+", "
 
-		generated_instr_code = generated_instr_code[:-2]
+		compute_code = compute_code[:-2]
 		
-		generated_instr_code += ";\\n\"\n"
+		compute_code += ";\\n\"\n"
 
-	generated_instr_code += ");\n"
+	compute_code += ");\n"
 	
 	# Put the generated instruction into the template code
-	template_code = template_code.replace("INSERT_CODE_HERE", generated_instr_code)
+	template_code = template_code.replace("COMPUTE_CODE", compute_code)
 
 	return template_code
 
@@ -41,20 +58,17 @@ def write_to_file(file_name, code):
 	f.close()
 
 def main():
+	parser = argparse.ArgumentParser()
+        parser.add_argument("-NI", help="number of instructions to be generated (default=1)", dest="nb_instr", action='store', default=1, type=int)
+        parser.add_argument("--hardware", help="generates hardware code (default=false)", action="store_true")
+        args = parser.parse_args()
 
-	nb_instr = 4096
+        nb_instr = args.nb_instr
 
-	# Check for input arg
-	if len(sys.argv) == 1:
-		print("Generating 4096 instructions")
-	else: 	
-		nb_instr = int(sys.argv[1])	#Number of instructions to be generated
-	
-	# Read the template code
-	# TODO: thread block size and grid are fixed now. (32 and 1 respectively)
-	f = open("template.tmp",'r')
-	template_code  = f.read()
-	f.close()
+        # Read the template code
+        f = open("template.tmp",'r')
+        template_code  = f.read()
+        f.close()
 
 	# For every instructions in the ISA, generate the benchmark
 	for instr in ISA_table:	
